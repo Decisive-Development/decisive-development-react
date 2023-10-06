3c834e29ffbc5eaa9f1c3637ba6859f498f2c51b
Decisive Development <decisive.development1@gmail.com>
2023-10-06 10:51:14 +0100

    Code clean

--- decisive-development/src/components/Heading.jsx
+++ decisive-development/src/components/Heading.jsx
@@ -14,21 +14,22 @@ const Heading = () => {
     <div className="relative z-20">
         <p className="text-2xl mb-5 font-light">TOM PENNY</p>
         <h1 className="text-7xl mb-5 font-medium flex h-20">
-            Website <span className=" ml-5">
-            <TypeAnimation
-                sequence={[
-                    'Developer', // Types 'One'
-                    1000, // Waits 1s
-                    'Two', // Deletes 'One' and types 'Two'
-                    2000, // Waits 2s
-                    'Two Three', // Types 'Three' without deleting 'Two'
-                    () => {
-                    console.log('Sequence completed');
-                    },
-                ]}
-                wrapper="span"
-                cursor={true}
-                repeat={Infinity}
+            Website 
+            <span className=" ml-5">
+                <TypeAnimation
+                    sequence={[
+                        'Developer', // Types 'One'
+                        1000, // Waits 1s
+                        'Two', // Deletes 'One' and types 'Two'
+                        2000, // Waits 2s
+                        'Two Three', // Types 'Three' without deleting 'Two'
+                        () => {
+                        console.log('Sequence completed');
+                        },
+                    ]}
+                    wrapper="span"
+                    cursor={true}
+                    repeat={Infinity}
                 />
             </span>
         </h1>
