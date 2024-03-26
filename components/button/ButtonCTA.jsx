bafcb7452ac65e768ea0b74dc7de410ff40fdfd1
Nick da silva <nickdasilva@ip-192-168-5-142.eu-west-2.compute.internal>
2024-03-26 20:30:44 +0000

    Example of how to use classix npm for combining classes on an element.

--- components/button/ButtonCTA.jsx
+++ components/button/ButtonCTA.jsx
@@ -1,25 +1,31 @@
 import Link from 'next/link';
+import cx from 'classix';
 
 const themes = {
-    red: "red border-theme-red",
-    blue: "blue border-theme-blue",
-    green: "green border-theme-green",
-    turquoise: "turquoise border-theme-turquoise",
-    orange: "orange border-theme-orange",
-    purple: "purple border-theme-purple"
+  red: 'red border-theme-red',
+  blue: 'blue border-theme-blue',
+  green: 'green border-theme-green',
+  turquoise: 'turquoise border-theme-turquoise',
+  orange: 'orange border-theme-orange',
+  purple: 'purple border-theme-purple'
 };
 
-
 const ButtonCTA = ({ href, children, theme, className }) => {
-    const baseClass = "bg-theme-primary text-white  font-semibold py-2 px-4 border rounded transition ease-in-out duration-300 relative z-20 w-40 block text-center font-code uppercase tracking-wider reactive duration-300";
-    const themeClass = themes[theme] || "";
-    const combinedClass = `${baseClass} ${themeClass} ${className || ''}`.trim();
+  const baseClass =
+    'bg-theme-primary text-white  font-semibold py-2 px-4 border rounded transition ease-in-out duration-300 relative z-20 w-40 block text-center font-code uppercase tracking-wider reactive duration-300';
 
-    return (
-        <a href={href} className={combinedClass}>
-            {children}
-        </a>
-    );
+  return (
+    <a
+      href={href}
+      className={cx(
+        baseClass,
+        themes[theme] && themes[theme],
+        className && className
+      )}
+    >
+      {children}
+    </a>
+  );
 };
 
 export default ButtonCTA;
