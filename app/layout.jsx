821cdc9db902bcc752e9b0891ee4bafa9231d2da
Decisive Development <decisive.development1@gmail.com>
2024-04-27 19:30:45 +0100

    added google A to website

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 // import { Inter } from 'next/font/google'
 import './globals.css'
-
+import GoogleAnalytics from './GoogleAnalytics';
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body >
+                <GoogleAnalytics />
                 {children}            
             </body>
         </html>
