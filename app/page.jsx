0a1e23bba36ee8bcb17f81c3ab0c200784ee2b36
Decisive Development <decisive.development1@gmail.com>
2024-05-12 05:57:03 +0100

    missed comment out

--- app/page.jsx
+++ app/page.jsx
@@ -15,17 +15,17 @@ import {
 export default function Home() {
   return (
     <main className="relative bg-theme-primary text-base">
-      <Nav />
-      <Header />
-      <Journey />
-      <Pricing />
-      {/* <Services /> */}
-      {/* <About /> */}
-      {/* <Projects /> */}
-      {/* <Partners /> */}
-      <Contact />
-      <Footer />
-      {/* <ParticlesComp /> */}
+        <Nav />
+        <Header />
+        <Journey />
+        <Pricing />
+        {/* <Services /> */}
+        {/* <About /> */}
+        {/* <Projects /> */}
+        {/* <Partners /> */}
+        <Contact />
+        <Footer />
+        <ParticlesComp />
     </main>
   );
 }
