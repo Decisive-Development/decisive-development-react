a3b6342df78a82db9826417190ac8aef6ad136a1
Decisive Development <decisive.development1@gmail.com>
2024-06-05 08:33:27 +0100

    test

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,13 +3,14 @@ import {
     Footer,
     ParticlesComp
 } from '../_components';
+import { FeedbackForm } from '../_components/contact/FeedbackForm';
 
-export default function Contact() {
+export default async function Contact() {
     return (
         <main className="relative bg-theme-primary text-base">
             <Nav />
             <div className='h-screen flex items-center justify-center'>
-                
+            <FeedbackForm />
             </div>
             <Footer />
             {/* <ParticlesComp /> */}
