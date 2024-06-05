c535ebcbd59a984a18d2c24d426bc1fd39d408d4
Decisive Development <decisive.development1@gmail.com>
2024-06-05 08:45:10 +0100

    test file

--- app/_components/contact/FeedbackForm.jsx
+++ app/_components/contact/FeedbackForm.jsx
@@ -14,7 +14,7 @@ export function FeedbackForm() {
             setError(null);
             const myForm = event.target;
             const formData = new FormData(myForm);
-            const res = await fetch('/public/_forms.html', {
+            const res = await fetch('/__forms.html', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
                 body: new URLSearchParams(formData).toString()
